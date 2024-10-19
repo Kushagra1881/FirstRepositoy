7b16a9af50499314d762dd07504280c9db1b8a4a
kusha <kushagrasrivastava08@gmail.com>
2024-10-19 10:22:46 +0530

    my first program

--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -1,7 +1,57 @@
+//object hello {
+//  def main(args:Array[String]):Unit=
+//  {
+//    println("hello world")
+//
+//  }
+//
+//}
+//
+
 object hello {
   def main(args:Array[String]):Unit=
   {
-    println("hello world")
+    //    println("hello world")
+    //    println("hello world.............")
+
+    //    var arr=Array(10,20,30,40,50,60,70,80,90,100)
+    //
+    //     val len=arr.length
+    //    print(len)
+    //
+    //      arr.foreach(println)
+    //
+    //   for(i<- 0 until 10){
+    //     print(arr(i))
+    //   }
+
+
+    //    var mylist=List(10,20,30,40,50,60,70,80,90,100)
+    //
+    //    print(mylist(9))
+    ////
+    ////    mylist.foreach(println)
+    //
+    //    for(i<-0 until mylist.length){
+    //      print(mylist(i))
+    //    }
+    //
+    //    print(mylist.head)
+    //
+    //
+    //    print("============================")
+    //    print(mylist.tail)
+
+
+    var myset=Set(10,20,30,40,50,50,50,50)
+
+    print(myset)
+
+
+
+
+
+
 
   }
 
